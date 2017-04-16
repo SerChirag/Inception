413398af0604d774e10669c7c4dee50a12d0c8d5
Chirag Vashist <chiragvashist007@gmail.com>
2017-04-16 06:13:34 +0530

    All the backend is ready...
    Type to display some things.

--- scripts/asker/script.js
+++ scripts/asker/script.js
@@ -12,6 +12,7 @@ var value = [];
 var xhr = [];
 var doc = [];
 var array = {};
+var type = {};
 
 function get_data(){
 	var count=0;
@@ -33,21 +34,71 @@ function get_data(){
 	    			}
 
 	    		}
-	    		if ((flag==1) || (newurl == 'chrome-extension:') || (newurl == 'facebook.com') || (newurl == 'google.co.in')){
+	    		if ((flag==1) || (newurl == 'chrome-extension:') || (newurl == 'facebook.com') || (newurl == 'google.co.in') || (newurl == 'twitter.com')){
 	    		
 	    		}
 	    		else{
-	    			
+	    			var awesomeurl = "https://nice-fx-116906.appspot.com/geturl?op=categorize&url=" + newurl;
+				        $.ajax({
+				          url: awesomeurl, success: function(result) {
+				            category = result.trim();
+				            var newTr = '<tr><td>' + category + '</td></tr>';
+				            $('#type_table').append(newTr);
+				        }
+				     });
 	    			array[count] = (newurl);
 	    			value[count] = 1;
 	    			count +=1;
 	    		}
 	    	}
 	    });
-	  
+	 chrome.history.search({text: '', maxResults: 200}, function(data) {
+	    data.forEach(function(page) {
+	    	
+    		var urlNoProtocol = page.url.replace(/^https?\:\/\//i, "");
+    		var newurl = urlNoProtocol.replace('www.', "");
+    		newurl = newurl.substring(0, newurl.indexOf('/'));
+
+    		var flag=0;
+    		for(i=0;i<5;i++){
+    			if(array[i]==newurl){
+    				value[i] +=1;
+    			}
+    		}
+	    });
+	    for(i=0;i<5;i++){
+	    	var fewTr = '<tr><td>' + value[i] + '</td></tr>';   
+	    	$('#frequency_table').append(fewTr);   
+		}
+	    
 	});
 
+	  console.log(array[0]);
+	  var i;
+	  for(i=0;i<5;i++){
+	  
+		  xhr[i] = new XMLHttpRequest();
+	   
+	      xhr[i].open('GET', 'http://www.alexa.com/siteinfo/'+array[i], true);
+	      //console.log(array[i]);
+	      xhr[i].responseType = 'document';
+	      xhr[i].send();
+	      xhr[i].onload = function(e) {  
+	          doc[i] = e.target.responseXML;
+	          
+	          var newTr = '<tr><td>' + doc[i].getElementById('traffic-rank-content').children[0].children[1].children[0].children[0].children[0].children[1].children[1].innerHTML.replace(/\D/g,'') + '</td></tr>';   
+	       	  $('#alexa_table').append(newTr);
+	       	  
+	       	  //console.log((doc[i].getElementById('traffic-rank-content').children[0].children[1].children[0].children[0].children[0].children[1].children[1].innerHTML).replace(/\D/g,''));   
+	             
+	      }
+	  }
+	});
+
+
 }
+
+
 
 /*
 function get_alexa(){
@@ -83,43 +134,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   document.getElementById('go_to').addEventListener('click', function() {
-  	var i = 0;
-    chrome.history.search({text: '', maxResults: 200}, function(data) {
-	    data.forEach(function(page) {
-	    	
-    		var urlNoProtocol = page.url.replace(/^https?\:\/\//i, "");
-    		var newurl = urlNoProtocol.replace('www.', "");
-    		newurl = newurl.substring(0, newurl.indexOf('/'));
-    		var flag=0;
-    		for(i=0;i<5;i++){
-    			if(array[i]==newurl){
-    				value[i] +=1;
-    			}
-
-    		}
-    		
-	    });
-	    console.log(value);  
-	    console.log(array);
-	});
-  	
-	/*for(i=0;i<2;i++){
-	  
-	  xhr[i] = new XMLHttpRequest();
-   
-      xhr[i].open('GET', 'http://www.alexa.com/siteinfo/'+array.i, true);
-      console.log(array.i);
-      xhr[i].responseType = 'document';
-      xhr[i].send();
-      xhr[i].onload = function(e) {  
-          doc[i] = e.target.responseXML;
-          
-          value[i] = ((doc[i].getElementById('traffic-rank-content').children[0].children[1].children[0].children[0].children[0].children[1].children[1].innerHTML).replace(/\D/g,''));   
-      
-      }
-	}*/
-
-
+  	console.log(value);
+  	  
   }, false);
+
 
 }, false);
